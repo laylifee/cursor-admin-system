65a3b0883725838e2b4f7a57a0b299d1f0f96bd2
liuoulin <18331501720@163.com>
2025-03-17 13:45:46 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { viteMockServe } from 'vite-plugin-mock'
-import { viteExternalsPlugin } from 'vite-plugin-externals'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
